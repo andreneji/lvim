2c60d7e93ec00f2d5a23e5576f06a8aab596a91b
andreneji <andreneji@gmail.com>
2022-04-02 18:56:14 -0300

    First commit

---
+++ lua/func.lua
@@ -0,0 +1,93 @@
+-- ┌─────────────────────────────────────────┐
+-- │        LvimFiles by André Lima          │
+-- ├─────────────────────────────────────────┤
+-- | email: andreneji@gmail.com              |
+-- | https://github.com/andreneji/lvim/      |
+-- └─────────────────────────────────────────┘
+
+-- ==== Functions ====
+
+-- Limelight settings
+vim.cmd([[
+    let g:limelight_conceal_ctermfg = 'gray'
+    let g:limelight_conceal_ctermfg = 240
+    let g:limelight_conceal_guifg = 'DarkGray'
+    let g:limelight_conceal_guifg = '#777777'
+]])
+
+-- "*** Goyo settings
+vim.cmd([[
+    function! s:goyo_enter()
+        if executable('tmux') && strlen($TMUX)
+          silent !tmux set status off
+          silent !tmux list-panes -F '\#F' | grep -q Z || tmux resize-pane -Z
+        endif
+        set noshowmode
+        set noshowcmd
+        set nocursorline
+        " CocDisable
+        Limelight
+        set cul
+        set number
+        set relativenumber
+    endfunction
+
+    function! s:goyo_leave()
+        if executable('tmux') && strlen($TMUX)
+          silent !tmux set status on
+          silent !tmux list-panes -F '\#F' | grep -q Z && tmux resize-pane -Z
+        endif
+        set showmode
+        set showcmd
+        set cursorline
+        " CocEnable
+        Limelight!
+        set cul
+        set number
+        set relativenumber
+    endfunction
+
+    autocmd! User GoyoEnter nested call <SID>goyo_enter()
+    autocmd! User GoyoLeave nested call <SID>goyo_leave()
+]])
+
+-- Replaces
+-- echom 'olá mundo!'
+-- a:    - argument da function
+-- rtype - replace type
+vim.cmd([[
+    function ReplaceAll(rtype)
+        if a:rtype == 'ask'
+          let a = input('Type a word: ')
+        else
+          let a = expand('<cword>')
+        endif
+
+        if !empty(a)
+          let b = input('Change "' . a . '" by: ')
+          if !empty(b)
+            let l = line('.')
+              if a:rtype == 'all' || a:rtype == 'ask'
+                execute '%s/' . a . '/' . b . '/g'
+              elseif a:rtype == 'line'
+                execute 's/' . a . '/' . b . '/g'
+              endif
+              execute l
+            endif
+        endif
+    endfunction
+]])
+
+-- " Removes trailing spaces
+vim.cmd([[
+    function TrimWhiteSpace()
+      %s/\s*$//
+      ''
+    :endfunction
+]])
+
+-- vim.cmd([[nmap  <leader><leader>  :call TrimWhiteSpace()<CR>]])
+-- vim.cmd([[nmap! <leader><leader>  :call TrimWhiteSpace()<CR>]])
+
+-- Removes trailing spaces before saving
+vim.cmd([[autocmd BufWritePre * :%s/\s\+$//e]])
---
+++ lua/map.lua
@@ -0,0 +1,155 @@
+-- ┌─────────────────────────────────────────┐
+-- │        LvimFiles by André Lima          │
+-- ├─────────────────────────────────────────┤
+-- | email: andreneji@gmail.com              |
+-- | https://github.com/andreneji/lvim/      |
+-- └─────────────────────────────────────────┘
+
+-- ==== Maps ====
+
+-- Multiline Statements
+-- (M=ALT e S=Shift)
+
+vim.cmd([[
+    nnoremap <c-a>ini       :vs $HOME/.config/lvim/config.lua<cr>
+    nnoremap <c-a>hac       :vs $HOME/.config/lvim/lua/hack.lua<cr>
+    nnoremap <c-a>map       :vs $HOME/.config/lvim/lua/map.lua<cr>
+    nnoremap <c-a>tmu       :vs $HOME/.tmux.conf<cr>
+    nnoremap <c-a>fun       :vs $HOME/.config/lvim/lua/func.lua<cr>
+    nnoremap <c-a>zsh       :vs $HOME/.zshrc<cr>
+]])
+
+-- Minimizar buffers, salvando sessão
+vim.cmd([[
+    nnoremap <c-a>mx        :mksession! ~/.session.vim<cr>:wincmd o<cr>
+    nnoremap <c-a>me        :mksession! ~/.session.vim<cr>
+    nnoremap <c-a>ml        :source ~/.session.vim<cr>
+]])
+
+-- Ocultar linhas (Toogle:za | all open/close:zr/zm | delete:zd)
+vim.cmd("xnoremap <c-a>f    :fold<cr>")
+
+-- Abrir e navegar entre tabs (abas)
+vim.cmd([[
+    nnoremap <c-a>.         :tabnext<cr>
+    nnoremap <c-a>,         :tabprev<cr>
+    nnoremap <c-a>c         :tabnew<cr>
+]])
+
+-- Miniminizar buffer atual
+vim.cmd("nnoremap <c-w>q  	:bun<cr>")
+
+-- Open new splits in a semantic way
+vim.cmd([[
+    nnoremap <c-w>h         :lefta vsp new <cr>
+    nnoremap <c-w>j         :bel sp new<cr>
+    nnoremap <c-w>k         :abo sp new<cr>
+    nnoremap <c-w>l         :rightb vsp new<cr>
+]])
+
+-- Redimensionar janelas
+vim.cmd([[
+    nnoremap <c-Up>         :resize +2<cr>
+    nnoremap <c-Down>       :resize -2<cr>
+    nnoremap <c-Left>       :vertical resize -2<cr>
+    nnoremap <c-Right>      :vertical resize +2<cr>
+]])
+
+-- Habilitar / desabilitar a quebra de linhas
+vim.cmd("nnoremap <c-a>w    :set wrap!<cr>")
+
+-- Fugitive + fzf + git
+vim.cmd([[
+    nnoremap <c-a>gj        :diffget //3<cr>
+    nnoremap <c-a>gf        :diffget //2<cr>
+    nnoremap <c-a>gs        :G<cr>
+]])
+
+-- Fzf-checkout - Manage branches and tags with fzf
+vim.cmd("nnoremap <c-a>gc   :GCheckout<cr>")
+
+-- Markdown
+vim.cmd("nmap <c-a>b        <Plug>MarkdownPreviewToggle")
+
+-- Esconder distrações
+vim.cmd("nnoremap <c-a>o    :Goyo<cr>")
+
+-- On/Off lights
+vim.cmd([[
+    nmap <c-a>ll           :Limelight<cr>
+    nmap <c-a>l0           :Limelight!<cr>
+]])
+
+-- Au[tocmd] capslock = scape quando entrar no vim e = capslock quando sair
+vim.cmd([[
+    au VimEnter * silent! !xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
+    au VimLeave * silent! !xmodmap -e 'clear Lock' -e 'keycode 0x42 = Caps_Lock'
+]])
+
+-- Changes
+vim.cmd([[
+    nnoremap <c-a>ra        :call ReplaceAll('all')<cr>
+    nnoremap <c-a>rl        :call ReplaceAll('line')<cr>
+    nnoremap <c-a>rw        :call ReplaceAll('ask')<cr>
+]])
+
+-- Altera a tecla CAPS LOCK p/ ESC
+vim.cmd([[
+    nmap <c-a>za            :silent! !xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'<cr>
+    nmap <c-a>zz            :silent! !xmodmap -e 'clear Lock' -e 'keycode 0x42 = Caps_Lock'<cr>
+]])
+
+-- Behave Vim
+vim.cmd("nnoremap Y y$")
+
+-- keeping it centered
+vim.cmd([[
+    nnoremap n nzzzv
+    nnoremap N Nzzzv
+    nnoremap J mzJ`z
+]])
+
+-- Undo break points
+vim.cmd([[
+    inoremap , ,<c-g>u
+    inoremap . .<c-g>u
+    inoremap ! !<c-g>u
+    inoremap ? ?<c-g>u
+]])
+
+-- Jumplist mulations
+vim.cmd([[
+    nnoremap <expr> k (v:count > 5 ? "m'" . v:count : "") . 'k'
+    nnoremap <expr> j (v:count > 5 ? "m'" . v:count : "") . 'j'
+]])
+
+-- Retira os ^M que ficam no final de arquivos salvos pelo windows
+vim.cmd("nnoremap <c-a>tt       :%s/\r//g")
+
+-- Minimiza / maximiza buffers fora do cursor
+-- vim.cmd("nnoremap <c-w>m        :MaximizerToggle!<cr>")
+-- By default it maps to `<F3>` in normal, insert, and visual modes.
+vim.cmd([[
+    let g:maximizer_set_mapping_with_bang = 1
+    nnoremap <c-w>m             :MaximizerToggle<CR>
+    vnoremap <c-w>m             :MaximizerToggle<CR>gv
+]])
+
+-- Ranger in a floatin window
+vim.cmd([[
+    nnoremap <silent> <c-a>j    :RnvimrToggle<CR>
+    tnoremap <silent> <c-a>j    <C-\><C-n>:RnvimrToggle<CR>
+]])
+
+-- Markdown
+-- nmap <C-s> <Plug>MarkdownPreview
+-- nmap <M-s> <Plug>MarkdownPreviewStop
+vim.cmd("nmap <C-p> <Plug>MarkdownPreviewToggle")
+
+-- Ḣabilita/desabilita o destaque de coluna
+vim.cmd("nmap <space><space>  :set cuc!<cr>")
+
+-- lua vim.lsp.buf.formatting()
+-- keymap("n", "<c-a>f", ":Format<cr>", opts)
+-- lvim.keys.normal_mode["<c-a>f"] = ":Format<CR>"
+lvim.keys.normal_mode["<c-a>f"] = "<cmd>lua vim.lsp.buf.formatting() <CR>"
